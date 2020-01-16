552b29d6b996b2f83609d9dcc644a244d82f1153
Ramesh Kumar <50226667+inforkgodara@users.noreply.github.com>
2020-01-16 23:17:22 +0100

    First commit

---
+++ InsertionSort.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+/**
+ * This is an implementation of the insertion sort algorithm.
+ * @author Ramesh Kumar
+ *
+ */
+public class InsertionSort {
+
+    /**
+     * Insertion sort implementation.
+     * @param list the array to be sorted.
+     * @param length the integer size of list.
+     */
+    static void sort(int list[], int length) {
+        for (int i = 1; i < length; ++i) { 
+            int key = list[i]; 
+            int j = i - 1; 
+            while (j >= 0 && list[j] > key) { 
+                list[j + 1] = list[j]; 
+                j = j - 1; 
+            } 
+            list[j + 1] = key; 
+        } 
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter length of list : ");
+        Scanner scanner = new Scanner(System.in);
+
+        int length = scanner.nextInt();
+        int[] list = new int[length];
+
+        for (int index = 0; index < length; index++) {
+            list[index] = scanner.nextInt();
+        }
+
+        sort(list, length);
+
+        System.out.print("Sorted list: ");
+
+        for (int element: list) {
+            System.out.print(element + " ");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Insertion Sort
+
+A simple single class implementation of insertion sort in Java. The user will be prompted to input the number of elements in a list array which they want to enter. The user then will choose a element to be sorted in the list. Insertion sort is a simple sorting algorithm that builds the final sorted array one item at a time. It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort.
+
+## How to use it.
+* Open a command prompt window and go to the directory where you saved the java program (InsertionSort.java).
+* Type 'javac InsertionSort.java' and press enter to compile your code. If there are no errors in your code, the command prompt will take you to the next line.
+* Now, type 'java InsertionSort' to run your program.
+* You will be able to see a output line and asking input to enter number of elements (integer) you want to enter.
+* Next you will enter number of element it can be space separated or in new line.
+* After completion of entering elements in list, program will show sorted list.
+
